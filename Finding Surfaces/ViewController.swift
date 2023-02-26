4532d4f7a4dade269058a9426cc1af411dbefb4a
Musatov Vladimir <vladimir.pmz@gmail.com>
2023-02-26 20:39:14 +0300

    add icons and screenshots

--- Finding Surfaces/ViewController.swift
+++ Finding Surfaces/ViewController.swift
@@ -5,28 +5,37 @@
 //  Created by Vladimir on 21.02.2023.
 //
 
-import UIKit
-import SceneKit
+
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+//MARK: - Outlets
     @IBOutlet var sceneView: ARSCNView!
+//MARK: - Methods
+    
+    /// add ship model
+    /// - Returns: ASCNode associated with the model
+    func loadShip()->SCNNode{
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let shipNode = scene.rootNode.clone()
+        return shipNode
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // enabled default lighting
+        sceneView.autoenablesDefaultLighting = true
+        
+        // set the debug options
+        sceneView.debugOptions = [.showWorldOrigin, .showFeaturePoints]
         
         // Set the view's delegate
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,6 +43,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -48,27 +58,41 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .horizontal{
+            // set size
+            let extent = planeAnchor.extent
+            let width = CGFloat(extent.x)
+            let height = CGFloat(extent.z)
+            
+            //create geometry
+            let plane = SCNPlane(width: width, height: height)
+            plane.firstMaterial?.diffuse.contents = UIColor.green
+            
+            // create node
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi/2
+            planeNode.opacity = 0.75
+            
+            // add node to the detected plane
+            node.addChildNode(planeNode)
+            node.addChildNode(loadShip())
+            
+            
+        }
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        if let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .horizontal{
+           
+            for childnode in node.childNodes {
+                childnode.simdPosition = planeAnchor.center
+                
+                if let plane = childnode.geometry as? SCNPlane{
+                    plane.width = CGFloat(planeAnchor.extent.x)
+                    plane.height = CGFloat(planeAnchor.extent.z)
+                }
+            }
+        }
     }
 }
